123576829b01758aa4c290069dd683026d2c6cf4
ronow2cn <594115727@qq.com>
2017-10-28 00:51:22 +0800

    build

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ export GOPATH=$(pwd)
 echo installing fujin...
 go install fujin
 
-cp SERVERS www.esiyou.com.key www.esiyou.com.pem config.json ctl.sh ./bin
+cp SERVERS config.json ctl.sh ./bin
 
 echo -e "\033[32mDone\033[0m"
