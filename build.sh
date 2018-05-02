ec313a81eb59fe227e7011a511ca56244f006d23
ronow2cn <594115727@qq.com>
2018-05-02 11:52:34 +0800

    build add filter

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ export GOPATH=$(pwd)
 echo installing fujin...
 go install fujin
 
-cp SERVERS config.json randname.txt ctl.sh ./bin
+cp SERVERS config.json filter.txt randname.txt ctl.sh ./bin
 
 echo -e "\033[32mDone\033[0m"
